1f2d4e746cce25e0e37b397fddd99f9999919e00
Gaurav Naval <gauravnaval003@gmail.com>
2026-04-05 23:57:00 +0530

    started with learning about how wsockets works in windows and how a tcp connection is started

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <WinSock2.h>
+
+using namespace std;
+
+int main() {
+
+    WORD wVersionRequested; // Version of Winsock we want to use
+    WSADATA wsaData ; // Structure to hold information about the Windows Sockets implementation
+    int err ;
+
+    // MAKEWORD(2,2) creates a 16-bit value that represents the version of Winsock we want to use (2.2 in this case)
+    wVersionRequested = MAKEWORD(2,2);
+
+    err = WSAStartup(wVersionRequested, &wsaData);
+
+    // If WSAStartup returns a non-zero value, it indicates an error occurred during initialization
+    if( err != 0 ) {
+        cout << "WSAStartup failed with error: " << err << endl;
+        return 1;
+    }
+
+    // Initialization succeeded
+    cout << "The Winsock 2.2 dll was found okay" << endl;
+
+    WSACleanup();
+
+    cout << "WSACleanup completed." << endl;
+
+    return 0;
+}
